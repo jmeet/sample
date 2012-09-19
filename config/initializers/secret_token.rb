626dac07892c649b7615938f2bf7cd97ac739e73
jmeet <jasmeets@cybage.com>
2012-09-20 01:05:01 +0530

    JASSI

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Post::Application.config.secret_token = '8f6c61cb2bac60efc9a1394aa4907bc16fa8dc97c93d3910dcf8693ef77d90f86bccd7fe0725f838ebbfab73f6e12ac18f6892c13da9fb0d14afe2236de8dc24'
